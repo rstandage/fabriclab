8ceaea321bacfbda8e11fd1ee871b3f9663aebab
rstandage <rstandage@juniper.net>
2023-10-20 10:58:40 +0100

    initial

---
+++ enable_lldp.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo '0x4000' > /sys/class/net/vmbr101/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr102/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr103/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr104/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr105/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr201/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr202/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr203/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr204/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr205/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr206/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr207/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr208/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr301/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr302/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr303/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr304/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr209/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr210/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr211/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr212/bridge/group_fwd_mask
+echo '0x4000' > /sys/class/net/vmbr500/bridge/group_fwd_mask
